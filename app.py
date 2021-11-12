df36a53de27b505e231bee78bc17de4172550719
Santhosh Raghul <gssanthoshraghul1010@gmail.com>
2021-11-13 04:52:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, redirect, send_from_directory, session
+import os
+# from wtforms.fields.core import FloatField, StringField
+# from flask_wtf import FlaskForm
+# from wtforms import IntegerField, SubmitField
+# import json
+# import shutil
+# from forms import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345678'
+
+@app.route('/test')
+def test():
+	return render_template('draw_graph.html', title="test")
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host="0.0.0.0")
